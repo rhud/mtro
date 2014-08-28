1f9f00aac3a9de38a3f5ddcbf0f7739eba6ef7f0
Ryan Hudson <ryan@hellohudson.com.au>
2014-08-28 13:15:48 +1000

    add apple images

--- templates/head.php
+++ templates/head.php
@@ -7,6 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php wp_head(); ?>
-
+  <link rel="apple-touch-icon" href="/assets/img/m.touch.icon.png">
+  <link rel="apple-touch-startup-image" href="/assets/img/startup.png">
+  
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
